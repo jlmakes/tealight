5647512d9fb9700e2b33eb508259b36f67469256
J.L. Makes <julianlloyd@gmail.com>
2018-01-03 00:20:47 +0100

    refactor utility fn tests

--- test/utils/is-node-list.spec.js
+++ test/utils/is-node-list.spec.js
@@ -1,19 +1,19 @@
 import isNodeList from '../../src/utils/is-node-list'
 
-describe('Utilities', () => {
-	describe('isNodeList()', () => {
-		it('should return true when passed a DOM node list', () => {
+describe('/utils/', () => {
+	describe('isNodeList(target)', () => {
+		it('— should return true when passed a NodeList', () => {
 			const result = isNodeList(document.querySelectorAll('script'))
 			expect(result).to.be.true
 		})
 
-		it('should return false when passed an array of HTML elements', () => {
+		it('— should return false when passed an Array<Node>', () => {
 			const elements = document.querySelectorAll('body')
 			const result = isNodeList(Array.prototype.slice.call(elements))
 			expect(result).to.be.false
 		})
 
-		it('should return false when passed anything else', () => {
+		it('— should return false when passed an invalid target', () => {
 			expect(isNodeList(null)).to.be.false
 			expect(isNodeList()).to.be.false
 			expect(isNodeList({})).to.be.false
--- test/utils/is-node.spec.js
+++ test/utils/is-node.spec.js
@@ -1,15 +1,22 @@
 import isNode from '../../src/utils/is-node'
 
-describe('Utilities', () => {
-	describe('isNode()', () => {
-		it('should return true when passed a DOM node', () => {
+describe('/utils/', () => {
+	describe('isNode(target)', () => {
+		it('— should return true when passed a Node', () => {
 			const result = isNode(document.querySelector('body'))
 			expect(result).to.be.true
 		})
 
-		it('should return false when passed HTML as a string', () => {
+		it('— should return false when passed HTML as a string', () => {
 			const result = isNode('<div class="foo"></div>')
 			expect(result).to.be.false
+		})
+
+		it('— should return false when passed an invalid target', () => {
+			expect(isNode(null)).to.be.false
+			expect(isNode()).to.be.false
+			expect(isNode({})).to.be.false
+			expect(isNode('!')).to.be.false
 		})
 	})
 })
