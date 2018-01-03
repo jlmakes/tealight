21b17d7c75267237fdd51387600a806fa9a5b78d
J.L. Makes <julianlloyd@gmail.com>
2018-01-03 14:49:39 +0100

    update test reporter formatting

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,55 +2,55 @@ import Tealight from '../src/index'
 
 describe('/', () => {
 	describe('index(target)', () => {
-		it('— should return an Array<Node> when passed an Array<Node>', () => {
+		it('should return an Array<Node> when passed an Array<Node>', () => {
 			const target = document.documentElement
 			const actual = [document.documentElement]
 			const result = Tealight(target)
 			expect(result).to.deep.equal(actual)
 		})
 
-		it('— should return an Array<Node> when passed an Array<Any>', () => {
+		it('should return an Array<Node> when passed an Array<Any>', () => {
 			const target = [document.documentElement, 'cookie', null]
 			const actual = [document.documentElement]
 			const result = Tealight(target)
 			expect(result).to.deep.equal(actual)
 		})
 
-		it('— should return an Array<Node> when passed a NodeList', () => {
+		it('should return an Array<Node> when passed a NodeList', () => {
 			const target = document.querySelectorAll('body')
 			const actual = Array.prototype.slice.call(target)
 			const result = Tealight(target)
 			expect(result).to.deep.equal(actual)
 		})
 
-		it('— should return an Array<Node> when passed a valid selector', () => {
+		it('should return an Array<Node> when passed a valid selector', () => {
 			const actual = [document.documentElement]
 			const result = Tealight('html')
 			expect(result).to.deep.equal(actual)
 		})
 
-		it('— should return an empty Array when nothing matches the selector', () => {
+		it('should return an empty Array when nothing matches the selector', () => {
 			const result = Tealight('.foo')
 			expect(result).to.deep.equal([])
 		})
 
-		it('— should return an empty Array when passed an invalid selector', () => {
+		it('should return an empty Array when passed an invalid selector', () => {
 			const result = Tealight('foo!')
 			expect(result).to.deep.equal([])
 		})
-		it('— should return an empty Array when passed an invalid target', () => {
+		it('should return an empty Array when passed an invalid target', () => {
 			const result = Tealight(null)
 			expect(result).to.deep.equal([])
 		})
 
-		it('— should return an empty Array when invoked without a target', () => {
+		it('should return an empty Array when invoked without a target', () => {
 			const result = Tealight()
 			expect(result).to.deep.equal([])
 		})
 	})
 
 	describe('index(target, context)', () => {
-		it('— should exclude all results not found within the context element', () => {
+		it('should exclude all results not found within the context element', () => {
 			const result = Tealight('html', document.body)
 			expect(result).to.deep.equal([])
 		})
--- test/utils/is-node.spec.js
+++ test/utils/is-node.spec.js
@@ -2,17 +2,17 @@ import isNode from '../../src/utils/is-node'
 
 describe('/utils/', () => {
 	describe('isNode(target)', () => {
-		it('— should return true when passed a Node', () => {
+		it('should return true when passed a Node', () => {
 			const result = isNode(document.querySelector('body'))
 			expect(result).to.be.true
 		})
 
-		it('— should return false when passed HTML as a string', () => {
+		it('should return false when passed HTML as a string', () => {
 			const result = isNode('<div class="foo"></div>')
 			expect(result).to.be.false
 		})
 
-		it('— should return false when passed an invalid target', () => {
+		it('should return false when passed an invalid target', () => {
 			expect(isNode(null)).to.be.false
 			expect(isNode()).to.be.false
 			expect(isNode({})).to.be.false
