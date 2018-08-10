ea15e24623f67f567f190199a673f96d8eff73c1
Julian Lloyd <julianlloyd@gmail.com>
2018-08-10 18:47:32 -0500

    add type definition

---
+++ src/index.d.ts
@@ -0,0 +1,23 @@
+export as namespace tealight;
+
+export = tealight;
+
+declare function tealight(
+    target: Array<any>,
+    context: HTMLElement
+): Array<HTMLElement>;
+
+declare function tealight(
+    target: string,
+    context: HTMLElement
+): Array<HTMLElement>;
+
+declare function tealight(
+    target: HTMLElement,
+    context: HTMLElement
+): Array<HTMLElement>;
+
+declare function tealight(
+    target: HTMLCollection,
+    context: HTMLElement
+): Array<HTMLElement>;
