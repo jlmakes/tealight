5e2ec3cbb6897698881fdd872445736a43771023
Julian Lloyd <julianlloyd@gmail.com>
2018-08-11 04:39:48 -0500

    update type declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,21 +3,6 @@ export as namespace tealight;
 export = tealight;
 
 declare function tealight(
-  target: Array<any>,
-  context?: HTMLElement
-): Array<HTMLElement>;
-
-declare function tealight(
-  target: string,
-  context?: HTMLElement
-): Array<HTMLElement>;
-
-declare function tealight(
-  target: HTMLElement,
-  context?: HTMLElement
-): Array<HTMLElement>;
-
-declare function tealight(
-  target: HTMLCollection,
+  target: string | HTMLElement | HTMLCollection | Array<any>,
   context?: HTMLElement
 ): Array<HTMLElement>;
